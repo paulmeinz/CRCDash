fa82d6039e6aded6c7772f046de3c014c7f0a29c
paulmeinz <paulmeinz@gmail.com>
2017-01-07 08:54:57 -0800

    feat: add a text render to test reactivity

--- app.R
+++ app.R
@@ -6,12 +6,20 @@ data <- read.csv('data.csv')
 termopt <- unique(data$term)
 subjopt <- unique(data$crs)
 radio <- c('gender','ethnicity')
+testing <- c("Fall, 2014" = 'Wrong',
+             "Spring, 2015" = 'Wrong',
+             "Fall, 2015" = 'Right!',
+             "Spring, 2016" = 'Wrong')
 
 ui <- fluidPage(
   checkboxGroupInput("term","Select a Term", choices = termopt),
-  textOutput(outputID = 'test')
+  textOutput("test")
 )
 
-server <- function(input, output) {}
+server <- function(input, output) {
+  output$test <- renderText ({
+    testing[input$term]
+  })
+}
 
 shinyApp(ui = ui, server = server)
