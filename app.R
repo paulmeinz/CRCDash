ca44b61639fcecf5ada117a124bf082bd81baae0
paulmeinz <paulmeinz@gmail.com>
2017-01-07 18:49:24 -0800

    feat: added two buttons and side panel format

--- app.R
+++ app.R
@@ -5,23 +5,34 @@ library(dplyr)
 data <- read.csv('data.csv')
 termopt <- unique(data$term)
 subjopt <- unique(data$crs)
-radio <- c('gender','ethnicity')
+radio <- c('Gender','Ethnicity')
 
 ui <- fluidPage(
-  checkboxGroupInput("strm","Select a Term", choices = termopt),
-  plotOutput("hist")
+  
+  titlePanel("HawkDash"),
+  sidebarLayout(
+    sidebarPanel(checkboxGroupInput("strm","Select a Term", choices = termopt),
+      radioButtons("demo","Select a Demographic", choices = radio),
+      actionButton("go", "Update")),
+    mainPanel(plotOutput("hist")))
 )
 
 server <- function(input, output) {
-  output$hist <- renderPlot ({
+  temp <- eventReactive(input$go, {
+    dots <- lapply(list("term",input$demo), as.symbol)
     
     temp <- data %>%
       subset(term %in% input$strm) %>%
-      group_by(term) %>%
+      group_by_(.dots = dots) %>%
       summarise(avg = mean(success))
     
-    ggplot(data = temp, aes(x = term, y = avg)) +
-      geom_bar(stat = 'identity')
+    names(temp)[2] <- "demo_col"
+    temp
+  })
+  
+  output$hist <- renderPlot ({
+    ggplot(data = temp(), aes(x = term, y = avg, fill = demo_col)) +
+      geom_bar(stat = "identity", position = position_dodge(), colour = "black")
     
   })
 }
