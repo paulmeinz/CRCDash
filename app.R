15c221e23b4863af1c43cb60cb4cbab2bdd73dac
paulmeinz <paulmeinz@gmail.com>
2017-01-08 18:39:56 -0800

    add a top and bottom row with some text formatting

--- app.R
+++ app.R
@@ -2,20 +2,37 @@ library(shiny)
 library(rCharts)
 library(dplyr)
 
-data <- read.csv('data.csv')
+# Grab data and calculate button/input values
+data <- read.csv("data.csv")
 termopt <- unique(data$term)
 subjopt <- unique(data$crs)
-radio <- c('Gender','Ethnicity')
+radio <- c("Gender","Ethnicity")
 
 ui <- fluidPage(
   
-  titlePanel("HawkDash"),
+  # Headers
+  fluidRow(
+    column(3, class = "top1",
+      tags$p(tags$h1(style = "color: Orange",
+                     "HawkDash"))),
+    column(9, class = "top2",
+      tags$p(
+        tags$h1(style = "color: Orange",
+                "Cosumnes River College Planning and Research")))),
+  
+  # Input buttons and plot area
   sidebarLayout(
     sidebarPanel(checkboxGroupInput("strm","Select a Term", choices = termopt),
       radioButtons("demo","Select a Demographic", choices = radio),
       checkboxGroupInput("sub","Subject Area", choices = subjopt),
       actionButton("go", "Update")),
-    mainPanel(showOutput("hist", lib = 'nvd3')))
+    mainPanel(showOutput("hist", lib = 'nvd3'))),
+  
+  # Bottom row with note about fabricated data
+  fluidRow(
+    column(12, class = "bottom",
+      tags$p(style = "color: Orange,","*Data are fabricated for demonstration"))
+  )
 )
 
 server <- function(input, output) {
@@ -37,7 +54,8 @@ server <- function(input, output) {
     n1$chart(color = c('blue', 'orange', 'brown', 'green', 'red'))
     n1$chart(forceY = c(0,100))
     n1$chart(tooltipContent = "#! function(key, x, y, e){ 
-      return '<h3>' + key + '</h3>' + '<strong>' + y + '%' + '</strong>' +' in ' + x
+      return '<p>' + '<strong>' + key + '</strong>' + '</p>' + 
+      '<strong>' + y + '%' + '</strong>' +' in ' + x
       } !#")
     n1$yAxis(axisLabel='Course Success Rate (%)', width=50)
     return(n1)
